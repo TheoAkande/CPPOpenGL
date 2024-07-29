d41e89a35baf2de804c0815713d9dbe476c4e7bd
Theo Akande <ta722@ic.ac.uk>
2024-07-29 14:06:13 +0100

    feat: chapter 4 program 1.3

--- src/chapter4/program4_1.cpp
+++ src/chapter4/program4_1.cpp
@@ -24,7 +24,7 @@ GLuint vbo[numVBOs];
 GLuint mvLoc, projLoc;
 int width, height;
 float aspect;
-glm::mat4 pMat, vMat, mMat, mvMat;
+glm::mat4 pMat, vMat, mMat, mvMat, tMat, rMat;
 
 void setupVertices(void) { 
     float vertexPositions[108] = {
@@ -57,6 +57,8 @@ void init(GLFWwindow* window) {
 
 void display(GLFWwindow* window, double currentTime) {
     glClear(GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
+
     glUseProgram(renderingProgram);
     // get the uniform variables for the MV and projection matrices
     mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
@@ -67,7 +69,16 @@ void display(GLFWwindow* window, double currentTime) {
     pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians = 60 degrees
     // build view matrix, model matrix, and model-view matrix
     vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
-    mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+
+    // use current time to compute different translations in x, y, and z
+    tMat = glm::translate(glm::mat4(1.0f),
+    glm::vec3(sin(0.35f*currentTime)*2.0f, cos(0.52f*currentTime)*2.0f, sin(0.7f*currentTime)*2.0f));
+    rMat = glm::rotate(glm::mat4(1.0f), 1.75f*(float)currentTime, glm::vec3(0.0f, 1.0f, 0.0f));
+    rMat = glm::rotate(rMat, 1.75f*(float)currentTime, glm::vec3(1.0f, 0.0f, 0.0f));
+    rMat = glm::rotate(rMat, 1.75f*(float)currentTime, glm::vec3(0.0f, 0.0f, 1.0f));
+    // the 1.75 adjusts the rotation speed
+    mMat = tMat * rMat;
+
     mvMat = vMat * mMat;
     // copy perspective and MV matrices to corresponding uniform variables
     glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
